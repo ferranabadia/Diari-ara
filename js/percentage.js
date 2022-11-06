ea2942b4e7c612ab6fc3a0c46592d48322e06ea1
ferran <ferranabadia@gmail.com>
2022-11-06 23:10:28 +0100

    End of DIARI ARA excercise

---
+++ js/percentage.js
@@ -0,0 +1,5 @@
+// ✅ Set width to Percentage  Candidate 1
+const percentage1 = document.getElementById('percentage1');
+percentage1.style.width = '51.2%';
+
+
---
+++ js/percentagemobile.js
@@ -0,0 +1,4 @@
+// ✅ Set width to Percentage  Candidate 1
+const percentage1mobile = document.getElementById('percentage1mobile');
+percentage1mobile.style.width = '51.8%';
+
